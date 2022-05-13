e67f2c226f6b8fd8f096d16dfc7e0e8bff4d42ad
Phillip Alexander <pal@jobindex.dk>
2022-05-13 14:56:41 +0200

    Move image cache to its own file

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"image/png"
 	"io"
 	"log"
-	"math/bits"
 	"net/http"
 	"net/url"
 	"os"
@@ -27,7 +26,6 @@ const (
 )
 
 var (
-	cacheTtl          time.Duration
 	maxImageSize      int
 	decapURL          string
 	signingSecret     string
@@ -39,9 +37,8 @@ var (
 var cache Cache
 
 func main() {
-	cacheTtlString, _ := getenv("CACHE_TTL", "12h")
-	var err error
-	cacheTtl, err = time.ParseDuration(cacheTtlString)
+	cacheTTLString, _ := getenv("CACHE_TTL", "12h")
+	cacheTTL, err := time.ParseDuration(cacheTTLString)
 	if err != nil {
 		log.Fatalf(`CACHE_TTL must be a valid duration such as "12h": %s\n`, err)
 	}
@@ -76,7 +73,7 @@ func main() {
 
 	http.HandleFunc("/", http.NotFound)
 	http.Handle(screenshotPath, http.HandlerFunc(screenshotHandler))
-	cache.Init()
+	cache.Init(cacheTTL)
 
 	fmt.Fprintf(os.Stderr,
 		"%s spectura is listening on http://localhost:%d%s\n",
@@ -97,88 +94,6 @@ func getenv(key string, fallback ...string) (string, error) {
 		return fallback[0], nil
 	}
 	return "", fmt.Errorf("missing environment variable %s", key)
-}
-
-// A CacheEntry wraps a PNG-encoded image to stored in a Cache. The screenshot
-// URL is used as the cache key.
-type CacheEntry struct {
-	Image []byte
-	URL   string
-	last  time.Time
-}
-
-// IsEmpty reports whether e is a zero value CacheEntry.
-func (e *CacheEntry) IsEmpty() bool {
-	return e.Image == nil && e.URL == ""
-}
-
-// A Cache is an in-memory key-value store of recently accessed CacheEntry
-// values. A new (zero value) Cache must be initialized before use (see Init).
-// Caches are safe for concurrent use by multiple goroutines.
-//
-// An entry that hasn't been requested for 12 hours is deleted from the Cache.
-type Cache struct {
-	entries               map[string]CacheEntry
-	readQuery             chan string
-	readReply, writeQuery chan CacheEntry
-}
-
-// Init initializes an existing Cache value for use through the Read and Write
-// methods.
-func (c *Cache) Init() {
-	*c = Cache{
-		entries:    make(map[string]CacheEntry),
-		readQuery:  make(chan string),
-		readReply:  make(chan CacheEntry),
-		writeQuery: make(chan CacheEntry),
-	}
-	go c.serve()
-}
-
-// Read returns the CacheEntry value at the given URL in the cache. If no entry
-// was found, a zero value entry is returned.
-func (c *Cache) Read(url string) CacheEntry {
-	c.readQuery <- url
-	return <-c.readReply
-}
-
-// Write writes a CacheEntry to the cache, using entry.URL as the key.
-func (c *Cache) Write(entry CacheEntry) {
-	c.writeQuery <- entry
-}
-
-func (c *Cache) serve() {
-	purge := time.NewTicker(5 * time.Minute)
-	for {
-		select {
-
-		case url := <-c.readQuery:
-			entry, exists := c.entries[url]
-			c.readReply <- entry
-			if exists {
-				entry.last = time.Now()
-				c.entries[url] = entry
-			}
-
-		case entry := <-c.writeQuery:
-			entry.last = time.Now()
-			c.entries[entry.URL] = entry
-
-		case <-purge.C:
-			size := 0
-			for url, entry := range c.entries {
-				elapsed := time.Since(entry.last)
-				if elapsed > cacheTtl {
-					delete(c.entries, url)
-					fmt.Fprintf(os.Stderr, "Clearing cache entry %s\n", url)
-				} else {
-					size += len(entry.Image)
-				}
-			}
-			fmt.Fprintf(os.Stderr, "%s %d images in cache (%s)\n",
-				time.Now().Format("[15:04:05]"), len(c.entries), fmtByteSize(size))
-		}
-	}
 }
 
 // Check a JIX::UrlSignature hash signature
@@ -290,15 +205,4 @@ type SubImager interface {
 
 func decapAction(list ...string) decap.Action {
 	return decap.Action(list)
-}
-
-func fmtByteSize(n int) string {
-	switch exp := bits.Len(uint(n)) / 10; exp {
-	case 0:
-		return fmt.Sprintf("%d B", n)
-	case 1, 2, 3, 4:
-		return fmt.Sprintf("%d %sB", n>>(exp*10), "KMGT"[exp-1:exp])
-	default:
-		return fmt.Sprintf("%d TB", n>>40)
-	}
 }
